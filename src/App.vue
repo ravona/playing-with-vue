1915261addaf02c1a3f8a9e3b475c32802ad3e89
Asaf Ravona <asafravona@gmail.com>
2021-08-04 22:35:28 +0300

    added sass-loader, and css reset

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <h1>AdIntel</h1>
   <img class="logo" alt="ironSource logo" src="./assets/logo.svg">
+  <h1 class="title">Vue.js app by Asaf Ravona</h1>
+  <Nav/>
   <router-view></router-view>
 </template>
 
 <script>
+import Nav from "./components/Nav.vue";
+
 export default {
   name: 'App',
   components: {
+    Nav,
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './style/style.scss';
+
 #app {
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 1rem;
+}
+
+.title {
+  color: $color-primary;
 }
 
 .logo {
   width: 20vw;
 }
+
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="nav">
+    <ul class="nav__menu">
+      <li class="nav__item">
+        <router-link to="/">Home</router-link>
+      </li>
+      <li class="nav__item">
+        <router-link to="/charts">Charts</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'ChartsPage'
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../style/colors';
+
+    .nav__menu {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    .nav__item {
+        font-size: 1.5rem;
+    }
+</style>
